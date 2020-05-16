35259e1f78fd2760b542f95ccf61c186a43940c3
Linze Zhao <Linze.Zhao@ncr.com>
2020-05-16 19:11:57 -0400

    formatting

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var letter = document.getElementsByClassName('letter');
 var mail = document.getElementsByClassName('mail');
 var pages = document.getElementById('pages');
 
-mail[0].addEventListener('click', function(event) {
+mail[0].addEventListener('click', function (event) {
     $('.background').fadeOut('slow');
     $('.mail').addClass('mail-scale');
     $('#pages').fadeIn('slow');
@@ -14,32 +14,44 @@ function pagination() {
     var offset = $(document).scrollTop();
     var windowHeight = $(window).height();
     var $body = $('body');
-  
+
     switch (true) {
-      case (offset > (windowHeight * 3.75)):
-        $body.removeClass().addClass('page-5');
-        break;
-      case (offset > (windowHeight * 2.75)):
-        $body.removeClass().addClass('page-4');
-        break;
-      case (offset > (windowHeight * 1.75)):
-        $body.removeClass().addClass('page-3');
-        break;
-      case (offset > (windowHeight * .75)):
-        $body.removeClass().addClass('page-2');
-        break;
-      default:
-        $body.removeClass().addClass('page-1');
-     }
-  }
-  
-  pagination();
-  
-  $(document).on('scroll', pagination);
-  
-  $(document).on('click', 'a[href^="#"]', function(e) {
-      e.preventDefault();
-      $('html, body').animate({
-          scrollTop: $($.attr(this, 'href')).offset().top
-      }, 500);
-  });
+        case (offset > (windowHeight * 7.75)):
+            $body.removeClass().addClass('page-9');
+            break;
+        case (offset > (windowHeight * 6.75)):
+            $body.removeClass().addClass('page-8');
+            break;
+        case (offset > (windowHeight * 5.75)):
+            $body.removeClass().addClass('page-7');
+            break;
+        case (offset > (windowHeight * 4.75)):
+            $body.removeClass().addClass('page-6');
+            break;
+        case (offset > (windowHeight * 3.75)):
+            $body.removeClass().addClass('page-5');
+            break;
+        case (offset > (windowHeight * 2.75)):
+            $body.removeClass().addClass('page-4');
+            break;
+        case (offset > (windowHeight * 1.75)):
+            $body.removeClass().addClass('page-3');
+            break;
+        case (offset > (windowHeight * .75)):
+            $body.removeClass().addClass('page-2');
+            break;
+        default:
+            $body.removeClass().addClass('page-1');
+    }
+}
+
+pagination();
+
+$(document).on('scroll', pagination);
+
+$(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
